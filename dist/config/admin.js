b67d6e286d3cc504279b174ec958dd275707c331
gianlucaparadise <gianlucaparadise@gmail.com>
2022-08-13 11:23:03 +0200

    Fix gitignore

--- dist/config/admin.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
-    auth: {
-        secret: env('ADMIN_JWT_SECRET', 'a9c6d3b94efdc7257b3f11e12c595c5b'),
-    },
-});
